0fc7c3e69688dffe047cf211a49bbc786da2127b
Henrik Samuelsson <henrik.samuelsson@gmail.com>
2017-06-04 16:51:46 +0200

    Copy paste of template to get an project for gpio experiments.

---
+++ efm32_projects/gpio_example/main.c
@@ -0,0 +1,13 @@
+
+#include "em_system.h"
+#include "em_chip.h"
+
+int main() {
+        
+        /* Chip errata */
+        CHIP_Init();
+        
+        while(1) {
+                
+        }
+}
